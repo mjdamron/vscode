4992380bffa7986e8f4b121d61a6bab51f4cf67d
Michael J. Damron <damronm@sbsnet.local>
2018-02-26 15:50:40 -0500

    First time commit, all new files added.

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models {
+    public class GuestResponse {
+        public int id {get; set;}
+
+        [Required(ErrorMessage = "Please enter your Name")]
+        public string Name {get; set;}
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email {get; set;}
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone {get; set;}
+
+        [Required(ErrorMessage = "Please specify whether you will attend")]
+        public bool? WillAttend {get; set;}
+
+    }
+}
